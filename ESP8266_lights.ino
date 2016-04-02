00a316da4a663550f3f02fd19da3100e92449537
John Cooper <john@choffee.co.uk>
2016-04-02 10:39:52 +0100

    Sketch to connect ESP01 to neopixel

---
+++ ESP8266_lights.ino
@@ -0,0 +1,88 @@
+
+
+/*
+ *  This sketch connect to a mqtt server and set the two neopixel lights connectd
+ *  based on the setting received.
+ *
+ */
+
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
+#include <ESP8266WiFi.h>
+#include "wireless_creds.h"
+
+// TODO: Move this to a .h config file
+const char* ssid     = WIRELESS_SSID;
+const char* password = WIRELESS_KEY;
+
+// Store the MQTT server, username, and password in flash memory.
+// This is required for using the Adafruit MQTT library.
+// TODO: Move this to a .h file 
+const char MQTT_SERVER[] PROGMEM    = "mqtt.local";
+
+const char MQTT_USERNAME[] PROGMEM  = MQTT_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM  = MQTT_PASSWORD;
+
+#define PIXEL_PIN 2 //GPIO 2
+#define PIXEL_COUNT 2
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, 1883, MQTT_USERNAME, MQTT_PASSWORD);
+
+// Setup a feed called 'color' for subscribing to changes.
+const char LIGHT_FEED[] PROGMEM = _MQTT_USERNAME "/lights/esplights/";
+Adafruit_MQTT_Subscribe lightscolor = Adafruit_MQTT_Subscribe(&mqtt, LIGHT_FEED);
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Setup MQTT subscription for onoff feed.
+  //mqtt.subscribe(&lightscolor);
+
+  pixels.begin();
+  pixels.setPixelColor(0, pixels.Color(0,150,0));
+  pixels.show();
+
+}
+
+int value = 0;
+
+void loop() {
+  delay(500);
+  ++value;
+  if (value > 255 ) { value = 0; };
+  pixels.setPixelColor(0, pixels.Color(0,value,0));
+  pixels.show();
+  
+}
---
+++ wireless_creds_example.h
@@ -0,0 +1,4 @@
+#define WIRELESS_SSID "yourssid"
+#define WIRELESS_KEY "deadbeef"
+#define MQTT_USERNAME "lights"
+#define MQTT_PASSWORD "lights"
